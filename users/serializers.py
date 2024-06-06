ecd5b753874166b264d99b136825c6bca08ac5dc
Akshita Jain <88099282+Akshita963@users.noreply.github.com>
2024-06-06 13:04:29 +0530

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,18 @@
-# api/serializers.py
+# serializers.py
 
 from rest_framework import serializers
 
 class UserSerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True)
-    username = serializers.CharField(max_length=150)
+    id = serializers.CharField(source='_id', read_only=True)
+    username = serializers.CharField()
     email = serializers.EmailField()
-    friends = serializers.ListField(child=serializers.EmailField(), default=[])
-    friend_requests = serializers.ListField(child=serializers.EmailField(), default=[])
+    friends = serializers.ListField(child=serializers.CharField(), required=False)
+    friend_requests = serializers.ListField(child=serializers.CharField(), required=False)
+
+    def create(self, validated_data):
+        return validated_data
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        return instance
