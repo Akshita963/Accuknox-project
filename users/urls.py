c36cc9a5089dfa019b57d52b040faa3dfdca3299
Akshita Jain <88099282+Akshita963@users.noreply.github.com>
2024-06-06 13:04:50 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
-# api/urls.py
 
 from django.urls import path
-from .views import signup, login, search_users, send_friend_request, handle_friend_request, list_friends, list_pending_requests
+from .views import signup, login, search_users, send_friend_request, handle_friend_request, list_friends, list_pending_requests,CookieTokenRefreshView,logout_view
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', signup),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('handle_friend_request/', handle_friend_request),
     path('list_friends/', list_friends),
     path('list_pending_requests/', list_pending_requests),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', logout_view, name='logout'),
 ]
